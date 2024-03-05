f6bc05a38a7ce043640dfad5851ad4a8f0b3dec5
Ehsan Fathalipour <www.ehsanfathalipour110@gmail.com>
2024-03-05 19:33:19 +0330

    sending dynamic data with useMutation

--- src/components/UseMutation.jsx
+++ src/components/UseMutation.jsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React , {useState} from "react";
 
 import { gql, useMutation } from "@apollo/client";
 
 const UseMutation = () => {
+    const[name,setName] = useState("");
+    const[username,setUsername] = useState("");
+    const[email,setEmail] = useState("");
+    const[phone,setPhone] = useState("");
   const CREATE_USER = gql`
-    mutation {
+    mutation createUser(
+      $name: String!
+      $username: String!
+      $email: String!
+      $phone: String!
+    ) {
       createUser(
         input: {
-          name: "ehsan"
-          username: "ef98"
-          email: "ehsan@me.com"
-          phone: "082329487298"
+          name: $name
+          username: $username
+          email: $email
+          phone: $phone
         }
       ) {
         id
@@ -21,12 +30,25 @@ const UseMutation = () => {
       }
     }
   `;
-  const [createUser, { loading, error, data, called }] =
-    useMutation(CREATE_USER);
+  const [createUser, { loading, error, data, called }] = useMutation(
+    CREATE_USER,
+    {
+      variables: {
+        name: name,
+        username: username,
+        email: email,
+        phone: phone,
+      },
+    }
+  );
   console.log({ loading, error, data, called });
 
   return (
     <div>
+        <input type="text" value={name} placeholder="enter name" onChange={(e)=>setName(e.target.value)} />
+        <input type="text" value={username} placeholder="enter username" onChange={(e)=>setUsername(e.target.value)} />
+        <input type="text" value={email} placeholder="enter email" onChange={(e)=>setEmail(e.target.value)} />
+        <input type="text" value={phone} placeholder="enter phone number" onChange={(e)=>setPhone(e.target.value)} />
       <button onClick={createUser}>CLICK TO LOAD THE INFO</button>
     </div>
   );
